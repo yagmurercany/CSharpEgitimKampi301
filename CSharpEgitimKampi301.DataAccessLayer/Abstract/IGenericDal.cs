c39e16f4d9c73bf8dd1fb5a733b4878769ab674c
Yağmur Ercan <yagmur.ercan5@icloud.com>
2025-02-27 22:07:56 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+
+        //ekleme silme işlemlerini entitiye göre yazdık 
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        List<T> GetAll();
+
+        T GetById(int id);
+    }
+}
